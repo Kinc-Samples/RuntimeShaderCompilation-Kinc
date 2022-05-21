6e33bf0302bd8d2dc87dcdd96dbe91b5245f7bbb
Robert Konrad <git2016@robdangero.us>
2022-05-21 15:48:04 +0200

    Add krafix as a library

--- kfile.js
+++ kfile.js
@@ -2,6 +2,8 @@ const project = new Project('ShaderTest');
 
 await project.addProject('Kinc');
 
+await project.addProject('krafix');
+
 project.addFile('Sources/**');
 project.addFile('Shaders/**');
 project.setDebugDir('Deployment');
